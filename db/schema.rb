a99fa9b8977a8c70204fa54a7c1254affffa1af8
sebagarcia <sebastian.garcia@alumnos.usm.cl>
2016-02-23 18:35:50 -0300

    segudno video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219214116) do
+ActiveRecord::Schema.define(version: 20160223151008) do
 
   create_table "publications", force: :cascade do |t|
     t.string   "price"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20160219214116) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
