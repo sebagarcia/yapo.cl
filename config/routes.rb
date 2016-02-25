58081f3cba36aecc8bce9588a8ccaf6e217493ec
sebagarcia <sebastian.garcia@alumnos.usm.cl>
2016-02-25 18:06:30 -0300

    video 14 de codigo facilito,autentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", 
-                                    registrations: "users/registrations",
-                                    passwords:"users/passwords",
-                                    sessions:"users/sessions"
+                                    registrations: "users/registrations",  
+                                    passwords: "users/passwords",
+                                    sessions: "users/sessions"
                                   }
   resources :publications
+
   root 'home#index'
+
 end
